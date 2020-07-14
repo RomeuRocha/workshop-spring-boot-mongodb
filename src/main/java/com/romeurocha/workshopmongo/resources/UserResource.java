b31a7d1345a76c01792e208d9f066f83270287aa
RomeuRocha <romeu.d.rocha@gmail.com>
2020-07-14 10:52:57 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/romeurocha/workshopmongo/resources/UserResource.java
+++ src/main/java/com/romeurocha/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.romeurocha.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.romeurocha.workshopmongo.domain.User;
 import com.romeurocha.workshopmongo.dto.UserDTO;
@@ -33,5 +36,22 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	//também pode usar @PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteById(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
