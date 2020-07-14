e7f842554aa2cb375288f462fb3e2c6e60a14d34
RomeuRocha <romeu.d.rocha@gmail.com>
2020-07-14 19:25:53 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/romeurocha/workshopmongo/domain/Post.java
+++ src/main/java/com/romeurocha/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.romeurocha.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.romeurocha.workshopmongo.dto.AuthorDTO;
+import com.romeurocha.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -26,6 +29,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		super();
@@ -78,6 +83,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
