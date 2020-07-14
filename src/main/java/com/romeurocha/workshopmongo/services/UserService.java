9b2b8534eacf58b846a7a1bfd9d69e541c327901
RomeuRocha <romeu.d.rocha@gmail.com>
2020-07-14 11:21:50 -0300

    Atualização de usuário com PUT

--- src/main/java/com/romeurocha/workshopmongo/services/UserService.java
+++ src/main/java/com/romeurocha/workshopmongo/services/UserService.java
@@ -34,6 +34,17 @@ public class UserService {
 		findById(id);
 		repository.deleteById(id);
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj,obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setName(obj.getName());
+	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
